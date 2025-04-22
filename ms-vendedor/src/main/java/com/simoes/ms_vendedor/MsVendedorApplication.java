3232a826aad7d88eb230bb1fc053d262577eafbb
DouglasSimoesM <douglas.simoesm@gmail.com>
2025-04-22 11:13:40 -0300

    first commit Microsserviço MarketFlow

---
+++ ms-vendedor/src/main/java/com/simoes/ms_vendedor/MsVendedorApplication.java
@@ -0,0 +1,13 @@
+package com.simoes.ms_vendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVendedorApplication.class, args);
+	}
+
+}
